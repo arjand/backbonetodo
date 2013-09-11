35c6a89058ff572c096d7756395024951464d4e5
Arjan Dehar <adehar@gmail.com>
2013-09-10 20:49:57 -0700

    Initial commit with default app from yeoman backbone generator

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ backbonetodo
 ============
 
 Simple Backbone Todo App
+
+Steps I took:
+
+1. Create empty repository on github
+2. Cloned to local machine
+3. Ran yeoman backbone generator `yo backbone`
+4. 
---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global backbonetodo, $*/
+
+
+window.backbonetodo = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    backbonetodo.init();
+});
