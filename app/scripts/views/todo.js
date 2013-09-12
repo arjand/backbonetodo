9ca2c2851a0e0515f1597b9b4f90be0e3943dc0d
Arjan Dehar <adehar@gmail.com>
2013-09-12 16:05:48 -0700

    Todo gets a strikethrough when it is checked off.

--- app/scripts/views/todo.js
+++ app/scripts/views/todo.js
@@ -11,19 +11,27 @@ backbonetodo.Views = backbonetodo.Views || {};
     backbonetodo.Views.TodoView = Backbone.View.extend({
 
     		tagName: 'li',
-    		arj: "foo",
+    		events: {
+    			'click input': 'toggleTodo'
+    		},    		
 
         template: JST['app/scripts/templates/todo.ejs'],
 
         initialize: function(){
         	this.listenTo(this.model, "change", this.render);
+        	this.listenTo(this.model, "change:checked", this.toggleChecked);
         },
 
         render: function() {
         	this.$el.html( this.template( this.model.toJSON()));
         	return this;
-        }
+        },
 
+        toggleTodo: function(e) {        	
+        	this.model.set({"checked" : !this.model.get("checked")});
+        },
+
+        // this.$el.toggleClass("todoCompleted");
     });
 
 })();
